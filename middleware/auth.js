3b771da0fd12c0ce2620c2cedc27efd0714fbaac
philippeeyraud <philippe.eyraud@ymail.com>
2022-08-12 16:27:54 +0200

    retour repository backend

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const jwt = require ('jsonwebtoken');
+
+module.exports =(req , res , next) => {
+   try {
+      const token = req.headers.authorization.split(' ')[1];
+      const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
+      const userId = decodedToken.userId;
+      req.auth = {
+         userId: userId
+       }; 
+      if (req.body.userId && req.body.userId !== userId) {
+         throw "requête non authentifiée !";
+     } else {
+         next();
+     }
+   }catch(error){
+    res.status(401).json({error});
+
+   }
+};
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const authCtrl = require('../controllers/auth');
+const password = require("../Utils/password");
+
+//pour intercepter les req post on crée une nouvelle instance de notre modele user.
+router.post('/signup',password,authCtrl.signup); 
+router.post('/login',authCtrl.login); 
+
+module.exports = router;
